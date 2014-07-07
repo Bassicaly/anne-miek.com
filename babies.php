3e3bb06de5fda4d0feadf0f163255c7ac7615e9d
Bassicaly <bas.de.bolster@gmail.com>
2014-07-07 11:04:21 +0200

    completed content addition
    Finished ading all deliverd content to the site

---
+++ babies.php
@@ -0,0 +1,46 @@
+<?php
+	$INC_DIR = $_SERVER["DOCUMENT_ROOT"]. "/inc/";
+	$title = "Anne-Miek Osteopathie Amsterdam";
+	$description = "Osteopaat Anne-Miek de Nijs in Amsterdam. Zeer bekend om haar osteopathie bij kinderen";
+?>
+
+<?php require($INC_DIR. "header.php"); ?>
+
+<?php require($INC_DIR. "logonav.php"); ?>
+
+	<section id="main_section">
+	<h2>Osteopathie bij baby’s</h2>
+	<p>Tijdens een bevalling worden grote krachten uitgeoefend op het lichaam van het ongeboren kind. De beweeglijkheid van het lichaampje en de schedel zorgen er in het algemeen voor dat deze krachten goed opgevangen kunnen worden. Maar als de bevalling erg lang heeft geduurd, of als deze juist heel snel is gegaan, dan kan dat gevolgen hebben voor het welzijn van het kind. Ook kunnen klachten ontstaan na een geboorte van een baby in stuitligging, na een vacuümverlossing of een keizersnede.</p>
+	<p>Een osteopaat kan dan op een zachte manier het lichaam van de baby doen ontspannen en zijn natuurlijke beweeglijkheid teruggeven. Er wordt beslist géén manipulaties uitgevoerd.</p>
+	<p>Het is onmogelijk een complete lijst te maken van alle klachten waarmee u met uw baby bij een osteopaat terecht kan. Hieronder vindt u een lijst van de klachten die wij het meest tegenkomen. Staat de klacht van uw kind er niet bij, aarzel dan niet contact op te nemen.</p>
+	<ul>
+		<li>Slaapproblemen</li>
+		<li>Problemen met voeden, zuigen of slikken</li>
+		<li>Darmkrampjes</li>
+		<li>Langdurig huilen</li>
+		<li>Refluxklachten, moeite met boeren</li>
+		<li>Voorkeurshouding</li>
+		<li>Astma</li>
+		<li>Eczeem en allergie&euml;n</li>
+		<li>Keel-, neus- en oorproblemen</li>
+	</ul>
+	<p>Osteopaten werken samen met reguliere geneeskundigen. Dankzij hun opleiding herkennen osteopaten klachten die nader onderzoek vereisen door bijvoorbeeld de huisarts of de specialist. Als het nodig is, verwijst de osteopaat naar hen door.</p>
+	</section>
+	
+	<aside id="news_left">
+		<ul>
+			<?php
+				$var = "test";
+				$als = "next";
+			?>
+		</ul>
+	</aside>
+
+	<aside id="news_right">
+		<ul>
+			
+		</ul>
+	</aside>
+
+<?php require($INC_DIR. "footer.php"); ?>
+
--- inc/logonav.php
+++ inc/logonav.php
@@ -14,16 +14,16 @@
 					<li><a href="/wanneernaardeosteo.php">Klachten</a></li>
 				</ul>
 			</li>
-			<li><a href="/nvo.php">Voor wie?</a>
+			<li><a href="#">Voor wie?</a>
 				<ul class="sub1">
-					<li><a href="/nvo.php">Babies</a></li>
-					<li><a href="/werkwijze.php">Kinderen</a></li>
-					<li><a href="/nvo.php">Zwangeren</a></li>
-					<li><a href="/nvo.php">Volwassenen</a></li>
+					<li><a href="/babies.php">Babies</a></li>
+					<li><a href="/kinderen.php">Kinderen</a></li>
+					<li><a href="/zwangerschap.php">Zwangeren</a></li>
+					<li><a href="/volwassenen.php">Volwassenen</a></li>
 				</ul>
 			</li>
-			<li><a href="/contact.php">Prijzen</a></li>
-			<li><a href="/nvo.php">Over mij</a></li>
+			<li><a href="/prijzen.php">Prijzen</a></li>
+			<li><a href="/anne-miek.php">Over mij</a></li>
 			<li><a href="/contact.php">Contact</a></li>
 		</ul>
 	</nav>

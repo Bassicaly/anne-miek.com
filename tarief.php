b66c744a9d6d8821e1e04ab91e2245aff1a755d6
Bassicaly <bas.de.bolster@gmail.com>
2014-07-25 22:27:08 +0200

    Moved BTW to separate file
    Moved BTW information into a separate file. In preperation of a news
    item

--- tarief.php
+++ tarief.php
@@ -19,9 +19,6 @@
 	<h2>Vergoeding door verzekeraar:</h2>
 	<p>Alle zorgverzekeraars vergoeden een osteopathisch consult geheel of gedeeltelijk uit het aanvullende pakket. Neem contact op met uw verzekeraar om te weten of dat ook geldt voor u. De meeste zorgverzekeraars vragen geen verwijzing voor osteopatische behandeling. <a href="http://www.osteopathie.nl/cms/upload/files/overzicht vergoedingen 2014-2 site.pdf" target="_blank">Hier (pdf)</a> vindt u een lijst van zorgverzekeraars en hun vergoedingen voor osteopathie voor 2014.</p>
 	<p>Ik ben aangesloten bij het <a href="http://www.osteopathie-nro.nl" target="_blank">Nederlands Register voor Osteopathie (NRO)</a> en de <a href="http://www.osteopathie.nl" target="_blank">Nederlandse vereniging voor Osteopathie (NVO)</a></p>
-	<h2>Verhoging BTW</h2>
-	<p>De regering heeft in het zogenaamde Kunduz- of lenteakkoord van 2012 besloten dat osteopaten 21% omzetbelasting moeten afdragen per 1-1-2013. Dit heeft voor onze beroepsgroep enorme gevolgen aangezien wij als zorgverleners tot op heden niet BTW-plichtig waren.</p>
-	<p>De beroepsvereniging van osteopaten, de NVO, stelt alles in het werk om deze maatregel teniet te doen. Echter tot op heden zonder resultaat. Wanneer u onze acties wilt steunen, kan dat via ondertekening van een petitie op: <a href="http://osteopathiebtwvrij.petities.nl" target="_blank">http://osteopathiebtwvrij.petities.nl/</a></p>
 	</section>
 	
 	<aside id="news_left">

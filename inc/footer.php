6065902ba2a8e07835f95b1366ae1cf1b40d6b48
Bassicaly <bas.de.bolster@gmail.com>
2016-01-17 20:23:20 +0100

    added stat counter to get better insight into usage

--- inc/footer.php
+++ inc/footer.php
@@ -1,5 +1,23 @@
 	<footer id="the_footer">
-		Designed and hosted at <span class="mylogo">Bolster Data Services</span>
+		Designed and hosted at <span class="mylogo">Bolster Data Services</span><br>
+		
+		<!-- Begin Web-Stat code v 6.2 -->
+		<span id="wts1109448">&nbsp;</span><script type="text/javascript">
+		var wtsh = {};
+		wtsh['invisible']='yes';
+		wtsh['text_counter']='#';
+		wtsh['page_name']='#';
+		wtsh['page_group']='#';
+		wtsh['conversion_number']='#';
+		wtsh['custom']='#';
+		var wts=document.createElement('script');wts.type='text/javascript';
+		wts.async=true;wts.src=('https:'==document.location.protocol?'https://server2':'http://lb')
+		+'.web-stat.com/4/1109448/log6_2.js';
+		document.getElementById('wts1109448').appendChild(wts);
+		</script><noscript><a href="http://www.web-stat.com">
+		<img src="//server2.web-stat.com/6/4/1109448.gif" 
+		style="border:0px;" alt=""></a></noscript>
+		<!-- End Web-Stat code v 6.2 -->
 	</footer>
 </div>
 <script>

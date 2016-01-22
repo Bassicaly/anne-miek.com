c77144cc65b11e2cd5f676ae761c740d9c040064
Bassicaly <bas.de.bolster@gmail.com>
2016-01-22 11:31:37 +0100

    removed web-stat from the site. Will look into different possibilities for tracking.

--- inc/footer.php
+++ inc/footer.php
@@ -1,16 +1,5 @@
 	<footer id="the_footer">
 		Designed and hosted at <span class="mylogo">Bolster Data Services</span><br>
-		
-		<!-- Begin Web-Stat code v 6.2 -->
-		<span id="wts1109486">&nbsp;</span><script type="text/javascript">
-		var wts=document.createElement('script');wts.type='text/javascript';
-		wts.async=true;wts.src=('https:'==document.location.protocol?'https://server2':'http://lb')
-		+'.web-stat.com/5/1109486/log6_2.js';
-		document.getElementById('wts1109486').appendChild(wts);
-		</script><noscript><a href="http://www.web-stat.com">
-		<img src="//server2.web-stat.com/6/5/1109486.gif" 
-		style="border:0px;" alt=""></a></noscript>
-		<!-- End Web-Stat code v 6.2 -->
 	</footer>
 </div>
 <script>

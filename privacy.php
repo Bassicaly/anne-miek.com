59b43fff24d09eda8a13c953f61fd7d8d58b3630
De Bolster <Bas.DeBolster@insight.com>
2019-06-05 21:26:35 +0200

    added privacy file

---
+++ privacy.php
@@ -0,0 +1,37 @@
+<?php
+	$INC_DIR = $_SERVER["DOCUMENT_ROOT"]. "/inc/";
+	$title = "UW PRIVACY";
+	$description = "Uw privacy met betrekking tot de nieuwe AVG en de WGBO";
+	$keywords = "AVG WGBO privacy gegevens beroepsgeheim";
+?>
+
+<?php require($INC_DIR. "header.php"); ?>
+
+<?php require($INC_DIR. "nav.php"); ?>
+
+	<section id="content">
+	<h2>UW PRIVACY</h2>
+	<p>Voor een goede behandeling is het noodzakelijk dat ik, als uw behandelende therapeut, een dossier aanleg. Dit is ook een wettelijke plicht opgelegd door de <a href="https://www.dwangindezorg.nl/rechten/wetten/wgbo" target="_blank">WGBO</a>. Uw dossier bevat aantekeningen over uw gezondheidstoestand en gegevens over de uitgevoerde onderzoeken en behandelingen.</p>
+	<p>Ook worden in het dossier gegevens opgenomen die voor uw behandeling noodzakelijk zijn en die ik, na uw expliciete toestemming, heb opgevraagd bij een andere zorgverlener bijvoorbeeld bij de huisarts.</p>
+	<p>Ik doe mijn best om uw privacy te waarborgen. Dit betekent onder meer dat ik:
+		<ul>
+			<li>zorgvuldig omgaan met uw persoonlijke en medische gegevens,</li>
+			<li>er voor zorg dat onbevoegden geen toegang hebben tot uw gegevens</li>
+		</ul>
+	</p>
+	<p>Als uw behandelende therapeut heb ik als enige toegang tot de gegevens in uw dossier. Ik heb een wettelijke geheimhoudingsplicht (beroepsgeheim).</p>
+	<p>De gegevens uit uw dossier kunnen ook nog voor de volgende doelen gebruikt worden:Om andere zorgverleners te informeren, bijvoorbeeld als de therapie is afgerond of bij een verwijzing naar een andere behandelaar.  Dit gebeurt alleen met uw expliciet toestemming. Voor het gebruik voor waarneming, tijdens mijn afwezigheid of voor het geanonimiseerde gebruik tijdens  intercollegiale toetsing. Een klein deel van de gegevens uit uw dossier wordt gebruikt voor de financiële administratie, zodat ik, of mijn administrateur een factuur kan opstellen. Als ik vanwege een andere reden gebruik wil maken van uw gegevens, dan zal ik u eerst informeren en expliciet uw toestemming vragen.</p>
+	<p>Deze gegevens in het cliëntendossier blijven zoals in de wet op de behandelovereenkomst wordt vereist 15 jaar bewaard.</p>
+	<h2>PRIVACY OP DE ZORGNOTA</h2>
+	<p>Op de zorgnota die u ontvangt staan de gegevens die door de zorgverzekeraar gevraagd worden, zodat u deze nota kan declareren bij uw zorgverzekeraar.
+		<ul>
+			<li>Uw naam, adres en woonplaats </li>
+			<li>uw geboortedatum</li>
+			<li>de datum van de behandeling</li>
+			<li>een korte omschrijving van de behandeling, zoals bijvoorbeeld ‘behandeling fysiotherapie’, of 'psychosociaal consult’</li>
+			<li>de tarief kosten</li>
+		</ul>
+	</p>
+	</section>
+	<?php require($INC_DIR. "footer.php"); ?>
+

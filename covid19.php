260dc592a2f999e29ab44456583e40ed4edd9f2a
Bas de Bolster <bas.de.bolster@gmail.com>
2020-04-17 15:24:19 +0200

    Updated site for COVID

---
+++ covid19.php
@@ -0,0 +1,19 @@
+<?php
+$INC_DIR = $_SERVER["DOCUMENT_ROOT"] . "/inc/";
+$title = "COVID 19 Update";
+$description = "Beperkte mogelijkheid om te werken gedurende covid 19/corona uitbraak";
+$keywords = "covid19 corona beperkte behandeling mogelijk";
+?>
+
+<?php require($INC_DIR . "header.php"); ?>
+
+<?php require($INC_DIR . "nav.php"); ?>
+
+<section id="content">
+	<h2>Beperkte mogelijkheid gedurende COVID 19</h2>
+	<br>
+	<h3>beste mensen,</h3>
+    ik mag weer onder bepaalde voorwaarden behandelen. Als u een cruciaal beroep heeft of verslechtering van uw klachten, kunt u contact met mij opnemen. We kunnen dan samen bepalen wat te beste strategie is.
+</section>
+
+<?php require($INC_DIR . "footer.php"); ?>

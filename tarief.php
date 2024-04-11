8a4e2cab203bedd9acc7b9e37eedf31c5f65baa8
Bas de Bolster <bas.de.bolster@gmail.com>
2024-04-11 22:18:45 +0200

    Further cleanup

--- tarief.php
+++ tarief.php
@@ -15,7 +15,7 @@
 	<p>1e consult van 1 uur: € 95,- </p>
 	<p>Vervolgconsulten van 45 minuten: € 88-</p>
 	<p>Consulten voor baby's en kinderen tot 12 jaar zijn 30 minuten: € 65,-</p>
-	<p>Betalingen per pin of contant. Bij ieder consult krijgt u een nota mee die u bij uw zorgverzekeraar kunt declareren.</p>
+	<p>Betalingen per pin of contant.</p>
 	<p>Annuleren: Indien U bij verhindering <strong>tenminste 24 uur van te voren afzegt</strong>, wordt de gereserveerde tijd niet in rekening gebracht.</p>
 	<h2>Vergoeding door verzekeraar:</h2>
 	<p>Alle zorgverzekeraars <a href="https://www.osteopathie.nl/download/974/Downloads/openbaar_gedeelte/231211-Ziektekostenvergoedingen-2024.pdf" target="_blank">vergoeden een osteopathisch consult geheel of gedeeltelijk uit het aanvullende pakket</a>. Neem contact op met uw verzekeraar om te weten of dat ook geldt voor u. De meeste zorgverzekeraars vragen geen verwijzing voor osteopatische behandeling. Hier is de pagina van de <a href="https://www.osteopathie.nl/vergoeding/kosten-en-vergoeding" target="_blank">Nederlandse Vereniging voor Osteopathie (NVO)</a> waar u onder anderen het kwaliteits register vindt en de vergoedingen die de verschillende zorgverzekeraars hebben opgenomen.</p>

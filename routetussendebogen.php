4f31270097c4a9f4aac04bf1b54a8e3620214d29
Bas de Bolster <bas.de.bolster@gmail.com>
2025-07-27 21:22:49 +0200

    Switching addresses

---
+++ routetussendebogen.php
@@ -0,0 +1,31 @@
+<?php
+$INC_DIR = $_SERVER["DOCUMENT_ROOT"] . "/inc/";
+$title = "Routebeschrijving Tussen de Bogen";
+$description = "routebeschrijving naar de praktijk op de Tussen de Bogen en parkeer gelegenheid in de omgeving";
+$keywords = "routebeschrijving Tussen de Bogen";
+?>
+
+
+<?php require($INC_DIR . "header.php"); ?>
+
+<?php require($INC_DIR . "nav.php"); ?>
+
+<section id="content">
+    <h2>Brouwersgracht</h2>
+    <ul>
+        <li>Tram 3: Uitstappen bij halte Haarlemmerplein</li>
+        <li>Bus 18, 21, 22: Uitstappen bij halte Buiten Oranjestraat of Haarlemmerplein</li>
+        <li>Vanaf beide haltes: Loop richting de spoorbogen (de bogen onder het spoorviaduct) en volg de straat Tussen de Bogen tot nummer 34.</li>
+        <li>Vanaf Centraal Station:
+            <ul>
+                <li>Neem bus 18, 21 of 22 richting westen en stap uit bij Buiten Oranjestraat of Haarlemmerplein.</li>
+                <li>Of loop in ongeveer 15 minuten via de Haarlemmerstraat en Haarlemmerplein naar Tussen de Bogen 34.</li>
+            </ul>
+        </li>
+    </ul>
+    <p>Parkeren: Tussen de Bogen heeft ook een eigen parkeergarage. Een plek reserveren is mogelijk via <a href="https://myway.app/"> de MyWay app</a>. De kosten zijn € 4,25 per uur op het moment van schrijven.</p>
+    <p>Verder is er een parkeergarage aan het Haarlemmerplein: Willemspoort (Haarlemmer Houttuin 549, 1013 GM). Kosten: € 0,50 per 7 minuten en een maximaal dagtarief van € 47,50.</p>
+    <p>Voor andere parkeermogelijkheden in de buurt, zie <a href="https://www.amsterdam.nl/parkeren/parkeertarieven/">deze link</a>.</p>
+</section>
+
+<?php require($INC_DIR . "footer.php"); ?>
